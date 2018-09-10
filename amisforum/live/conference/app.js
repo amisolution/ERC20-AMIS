62e40e19024dc48a132d5ba2901b126584b20071
amisolution <info@amisolution.net>
2018-09-10 10:15:56 +0200

    cleanup

--- amisforum/live/conference/app.js
+++ amisforum/live/conference/app.js
@@ -1,32 +1,31 @@
-
-
 metamaskIsInstalled();
 
 console.log(web3.isConnected());
 
-var accounts, account;
-var myConferenceInstance;
 abi = JSON.parse('[{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"registrantsPaid","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"organizer","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"recipient","type":"address"},{"name":"amount","type":"uint256"}],"name":"refundTicket","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"destroy","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newquota","type":"uint256"}],"name":"changeQuota","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"quota","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"numRegistrants","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"buyTicket","outputs":[],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_from","type":"address"},{"indexed":false,"name":"_amount","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_to","type":"address"},{"indexed":false,"name":"_amount","type":"uint256"}],"name":"Refund","type":"event"}]');
 var Conference = web3.eth.contract(abi);
-
-var contractAddress = '0x8F3f14d8C8db1D544404Ab2A2273dB97740a8eb7';
+var contractAddress = '0x7fe180e63198e50057c3f2817d058e8904c650e3';
 var contractInstance = Conference.at(contractAddress);
-var etherscanUrl = "https://ropsten.etherscan.io/address/0xec4e3a1ad06a2ebdc462eaf6bf361404676a441f#code";
+
+var etherscanTxUrl = "https://etherscan.io/tx/0x57d9096861063b468194362d08cf7bffca656ea62db3c4cdaa558c124f2311db";
+var etherscanUrl = "https://etherscan.io/address/0x7fe180e63198e50057c3f2817d058e8904c650e3#code";
+
+
 web3.version.getNetwork((err, netId) => {
     switch (netId) {
         case "1":
         $('#network').text("");
-        contractAddress = '0x8F3f14d8C8db1D544404Ab2A2273dB97740a8eb7';
+        contractAddress = '0x7fe180e63198e50057c3f2817d058e8904c650e3';
         break;
         case "2":
         alert('This is the deprecated Morden test network.');
         break;
         case "3":
         $('#network').text("ROPSTEN TEST NETWORK");
-        contractAddress = '0xEc4e3A1Ad06A2EbDC462eAf6Bf361404676a441F';
+        contractAddress = '0xec4e3a1ad06a2ebdc462eaf6bf361404676a441f';
         contractInstance = Conference.at(contractAddress);
         etherscanTxUrl = "https://ropsten.etherscan.io/tx/0x88799ff0c66cb9e95df05f968c3a8bce86bd50c1774ba29d6eadea7138f5fa79";
-        etherscanUrl = "https://ropsten.etherscan.io/address/0xEc4e3A1Ad06A2EbDC462eAf6Bf361404676a441F#code";
+        etherscanUrl = "https://ropsten.etherscan.io/address/0xec4e3a1ad06a2ebdc462eaf6bf361404676a441f#code";
         break;
         case "4":
         $('#network').text("RINKEBY TEST NETWORK");
@@ -36,8 +35,8 @@ web3.version.getNetwork((err, netId) => {
         $('#network').text("KOVAN TEST NETWORK");
         contractAddress = '0x6328426be01834de85ec0c10e3e940e629290bb1';
         contractInstance = Conference.at(contractAddress);
-        etherscanTxUrl = "https://kovan.etherscan.io/tx/0x1d5d170d9277db72d9bd81641b4c1324bfc4975ec8a9840543f0c2a091c19f96";
-        etherscanUrl = "https://kovan.etherscan.io/address/0x6328426be01834de85ec0c10e3e940e629290bb1#code";
+        etherscanTxUrl = "https://kovan.etherscan.io/tx/";
+        etherscanUrl = "https://kovan.etherscan.io/address/#code";
         //TODO ADD contractAddress = kovan address
         https://kovan.etherscan.io/address/0x6328426be01834de85ec0c10e3e940e629290bb1
         break;
@@ -46,6 +45,31 @@ web3.version.getNetwork((err, netId) => {
         //TODO ADD contractAddress = undefined
     }
 })
+
+var ticket_abi = ticket_abi = JSON.parse('[{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"registrantsPaid","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"organizer","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"recipient","type":"address"},{"name":"amount","type":"uint256"}],"name":"refundTicket","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"destroy","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newquota","type":"uint256"}],"name":"changeQuota","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"quota","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"numRegistrants","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"buyTicket","outputs":[],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_from","type":"address"},{"indexed":false,"name":"_amount","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_to","type":"address"},{"indexed":false,"name":"_amount","type":"uint256"}],"name":"Refund","type":"event"}]');
+var ticket = web3.eth.contract(ticket_abi);
+
+var decimals = undefined;
+var name = undefined;
+var symbol = undefined;
+var allowance = undefined;
+var safeLowGasPrice = setSafeLowGasPrice();
+var registrantsPaid = getRegistrantsPaid();
+var organizer = getOrganizer();
+var numRegistrants = getNumRegistrants();
+var quota = getQuota();
+var ticketIsValid = true;
+
+
+setInterval(function() {
+    getOrganizer();
+    getNumRegistrants();
+    getQuota();
+    getTickets();
+    total = quota - numRegistrants;
+    setSafeLowGasPrice();
+}, 1000);
+
 
 function metamaskIsLocked() {
     return web3.eth.accounts.length == 0;
@@ -67,37 +91,37 @@ function metamaskIsInstalled() {
 
 // Initialize
 function initializeConference() {
-	Conference.web3({from: accounts, gas: 3141592}).then(
+	contractInstance({from: accounts, gas: 3141592}).then(
 	function(conf) {
 		console.log(conf);
-		myConferenceInstance = conf;
-		$("#confAddress").html(myConferenceInstance.address);
+		contractInstance = conf;
+		$("#confAddress").html(contractInstance.address);
 		checkValues();
 	});
 }
 
 // Check Values
 function checkValues() {
-	myConferenceInstance.quota.call().then(
+	contractInstance.quota.call().then(
 		function(quota) { 
 			$("input#confQuota").val(quota);
-			return myConferenceInstance.organizer.call();
+			return contractInstance.organizer.call();
 	}).then(
 		function(organizer) { 
 			$("input#confOrganizer").val(organizer);
-			return myConferenceInstance.numRegistrants.call(); 
+			return contractInstance.numRegistrants.call(); 
 	}).then(
 		function(num) { 
 			$("#numRegistrants").html(num.toNumber());
-			return myConferenceInstance.organizer.call();
+			return contractInstance.organizer.call();
 	});
 }
 
 // Change Quota
 function changeQuota(val) {
-	myConferenceInstance.changeQuota(val, {from: accounts}).then(
+	contractInstance.changeQuota(val, {from: accounts}).then(
 		function() {
-			return myConferenceInstance.quota.call();
+			return contractInstance.quota.call();
 		}).then(
 		function(quota) {
 			if (quota == val) {
@@ -113,13 +137,13 @@ function changeQuota(val) {
 // buyTicket
 function buyTicket(buyerAddress, ticketPrice) {
 
-	myConferenceInstance.buyTicket({ from: buyerAddress, value: ticketPrice }).then(
+	contractInstance.buyTicket({ from: buyerAddress, value: ticketPrice }).then(
 		function() {
-			return myConferenceInstance.numRegistrants.call();
+			return contractInstance.numRegistrants.call();
 		}).then(
 		function(num) {
 			$("#numRegistrants").html(num.toNumber());
-			return myConferenceInstance.registrantsPaid.call(buyerAddress);
+			return contractInstance.registrantsPaid.call(buyerAddress);
 		}).then(
 		function(valuePaid) {
 			var msgResult;
@@ -137,19 +161,19 @@ function refundTicket(buyerAddress, ticketPrice) {
 
 		var msgResult;
 
-		myConferenceInstance.registrantsPaid.call(buyerAddress).then(
+		contractInstance.registrantsPaid.call(buyerAddress).then(
 		function(result) {
 			if (result.toNumber() == 0) {
 				$("#refundTicketResult").html("Buyer is not registered - no refund!");
 			} else {		
-				myConferenceInstance.refundTicket(buyerAddress, 
+				contractInstance.refundTicket(buyerAddress, 
 					ticketPrice, {from: accounts[0]}).then(
 					function() {
-						return myConferenceInstance.numRegistrants.call();
+						return contractInstance.numRegistrants.call();
 					}).then(
 					function(num) {
 						$("#numRegistrants").html(num.toNumber());
-						return myConferenceInstance.registrantsPaid.call(buyerAddress);
+						return contractInstance.registrantsPaid.call(buyerAddress);
 					}).then(
 					function(valuePaid) {
 						if (valuePaid.toNumber() == 0) {
