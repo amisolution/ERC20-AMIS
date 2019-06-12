6fb795fbf771c2bfeb43029a867f6eef96e958e3
amisolution <siriusgem1@gmail.com>
2019-06-13 00:06:41 +0200

    Update custom.js

--- amisforum/live/js/custom.js
+++ amisforum/live/js/custom.js
@@ -34,9 +34,9 @@ var x = setInterval(function () {
 
 
   // If the count down is over, write some text 
-  if (distance < 0) {
+  if (circulatingSupply > 0) {
     clearInterval(x);
- /*    document.getElementById("demo").innerHTML = "EXPIRED"; */
+     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
 
